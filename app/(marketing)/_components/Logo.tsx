0d4eb5942f849acf82db713ef1eefe0414bf6dd8
Kribas <carlousrimal@gmail.com>
2023-10-27 16:40:08 +0545

    feat: created footer component

---
+++ app/(marketing)/_components/Logo.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export default function Logo() {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image src="/logo.svg" alt="Logo" width="40" height="40" />
+      <p className={cn("font-semibold", font.className)}>Jotion</p>
+    </div>
+  );
+}
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Heading from "./_components/Heading";
+import Heroes from "./_components/Heroes";
+import Footer from "./_components/Footer";
 
 export default function MarketingPager23W() {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-1 items-center justify-center flex-col md:justify-start text-center gap-y-8 p-6 pb-10">
         <Heading />
+        <Heroes />
       </div>
+      <Footer />
     </div>
   );
 }
