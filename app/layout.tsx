0247b5d1f71f5ef15c04dc67b6d543bfe29bbecc
Kribas <carlousrimal@gmail.com>
2023-11-02 16:17:56 +0545

    made changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Toaster } from "sonner";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
