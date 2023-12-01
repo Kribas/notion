b58814162dc4750d7f4d7d2337de9264bd6cff36
Kribas <carlousrimal@gmail.com>
2023-12-01 11:05:22 +0545

    initial commit

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,6 +1,11 @@
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronLeft, LucideIcon } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  LucideIcon,
+} from "lucide-react";
 import React from "react";
 
 interface ItemProps {
@@ -28,7 +33,7 @@ export const Item: React.FC<ItemProps> = ({
   icon: Icon,
   onClick,
 }) => {
-  const chevronIcon = expanded ? ChevronDown : ChevronLeft;
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
     <div
@@ -42,6 +47,15 @@ export const Item: React.FC<ItemProps> = ({
         active && "bg-primary/5 text-primary"
       )}
     >
+      {!!id && (
+        <div
+          onClick={() => {}}
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+          role="button"
+        >
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
+        </div>
+      )}
       <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
       <span className="truncate">{label}</span>
     </div>
