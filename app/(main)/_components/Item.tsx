b14ba4aef436b1b31e01d49c66af74000160fd85
Kribas <carlousrimal@gmail.com>
2023-12-08 18:14:35 +0545

    feat: implemented recursive parent child itemList on sidenav dropdown

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,13 +1,18 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useMutation } from "convex/react";
 import {
   ChevronDown,
   ChevronLeft,
   ChevronRight,
   LucideIcon,
+  Plus,
 } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -34,9 +39,36 @@ export const Item: React.FC<ItemProps> = ({
   icon: Icon,
   onClick,
 }) => {
-  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+  const create = useMutation(api.documents.create);
+  const router = useRouter();
 
-  console.log("Initial commit");
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    onExpand?.();
+  };
+
+  const onCreate = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = create({ title: "Untitled", parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        // router.push(`/documents/${documentId}`);
+      }
+    );
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      error: "Failed to create new note",
+    });
+  };
+
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
     <div
@@ -52,7 +84,7 @@ export const Item: React.FC<ItemProps> = ({
     >
       {!!id && (
         <div
-          onClick={() => {}}
+          onClick={handleExpand}
           className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
           role="button"
         >
@@ -73,6 +105,18 @@ export const Item: React.FC<ItemProps> = ({
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">CTRL</span>K
         </kbd>
+      )}
+
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   );
