f51a72d28bf2b63c50458b115adf39feccb5f1af
Kribas <carlousrimal@gmail.com>
2024-01-17 14:04:16 +0545

    changes: made changes to menu component

--- app/(main)/_components/Menu.tsx
+++ app/(main)/_components/Menu.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface MenuProps {
   documentId: Id<"documents">;
 }
 
 export const Menu = ({ documentId }: MenuProps) => {
+  const user = useUser();
+  const archive = useMutation(api.documents.archive);
+  const router = useRouter();
+  const onArchive = () => {
+    const promise = archive({ id: documentId });
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Failed to archive note.",
+    });
+    router.push("/documents");
+  };
+
   return <div>Menu</div>;
 };
