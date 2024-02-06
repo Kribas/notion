3478d563116d9f7368c1c615530e62f4207162aa
Kribas <carlousrimal@gmail.com>
2024-02-06 21:02:49 +0545

    feat:add and remove icon convex api

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 import { ImageIcon, Smile, X } from "lucide-react";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import TextareaAutosize from "react-textarea-autosize";
 import { ElementRef, useRef, useState } from "react";
 
 interface ToolbarProps {
@@ -18,6 +19,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
   const inputRef = useRef<ElementRef<"textarea">>(null);
   const [isEditing, setIsEditing] = useState(false);
   const [value, setValue] = useState(initialData.title);
+  const removeIcon = useMutation(api.documents.removeIcon);
 
   const enableInput = () => {
     if (preview) return;
@@ -33,18 +35,45 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
     setIsEditing(false);
   };
 
-  console.log("initial commit");
+  const onInputValue = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key == "Enter") {
+      event.stopPropagation();
+      disableInput();
+    }
+  };
+
+  const onIconSelect = (icon: string) => {
+    update({
+      id: initialData._id,
+      icon,
+    });
+  };
+
+  const onRemoveIcon = () => {
+    removeIcon({
+      id: initialData._id,
+    });
+  };
 
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
         <div className="flex items-center gap-x-2 group/icon pt-6">
-          <IconPicker onChange={() => {}}>
+          <IconPicker onChange={onIconSelect}>
             <p className="text-6xl hover:opacity-75 transition">
               {initialData.icon}
             </p>
           </IconPicker>
           <Button
+            onClick={onRemoveIcon}
             className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
             variant="outline"
             size="icon"
@@ -58,7 +87,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
       )}
       <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
         {!initialData.icon && !preview && (
-          <IconPicker asChild onChange={() => {}}>
+          <IconPicker asChild onChange={onIconSelect}>
             <Button
               className="text-muted-foreground text-xs"
               variant="outline"
@@ -81,6 +110,24 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
           </Button>
         )}
       </div>
+      {isEditing && !preview ? (
+        <div>
+          <TextareaAutosize
+            ref={inputRef}
+            onBlur={disableInput}
+            onChange={(e) => onInputValue(e.target.value)}
+            onKeyDown={onKeyDown}
+            className="text-5xl bg-transparent font-bold break-words outline-none text-[#3f3f3f] dark:text-[#cfcfcf] resize-none"
+          />
+        </div>
+      ) : (
+        <div
+          onClick={enableInput}
+          className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3f3f3f] dark:text-[#cfcfcf]"
+        >
+          {initialData.title}
+        </div>
+      )}
     </div>
   );
 };
