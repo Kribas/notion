28515a5f10c8bd25ededa4d844ba20450f712e53
Kribas <carlousrimal@gmail.com>
2024-03-04 11:27:48 +0545

    fix:fixed remove cover image from convex db issue

--- components/cover.tsx
+++ components/cover.tsx
@@ -9,6 +9,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverProps {
   url?: string;
@@ -18,9 +19,15 @@ interface CoverProps {
 export const Cover = ({ url, preview }: CoverProps) => {
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
+  const { edgestore } = useEdgeStore();
   const params = useParams();
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      });
+    }
     removeCoverImage({
       id: params.documentId as Id<"documents">,
     });
